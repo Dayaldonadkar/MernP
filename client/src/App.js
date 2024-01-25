f3d0b2a2f1ca1686b964859deaaffeae3cc6a3a8
Dayal Sudhakar <Dayalpython@gmail.com>
2024-01-26 02:10:22 +0530

    responsive navbar and react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Component/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
+import Contact from "./Pages/Contact";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [showNavbar, setshowNavbar] = useState(false);
   return (
@@ -8,11 +9,21 @@ const Navbar = () => {
         <h1>Dayal</h1>
         <div className="hidden lg:block">
           <ul className="flex space-x-10">
-            <li>Home</li>
-            <li>About me</li>
-            <li>Contact</li>
-            <li>Login</li>
-            <li>Register</li>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About me</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact</li>
+            </Link>
+            <Link to="/login">
+              <li>Login</li>
+            </Link>
+            <Link to="/register">
+              <li>Register</li>
+            </Link>
           </ul>
         </div>
 
