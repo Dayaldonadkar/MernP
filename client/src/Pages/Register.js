22bdf48c0542d4b14c6248fee3c1c2936a913cd8
Dayal Sudhakar <Dayalpython@gmail.com>
2024-01-26 17:59:03 +0530

    contact page resoponsive added

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -25,13 +25,6 @@ const Register = () => {
         <div className="container px-4 mx-auto">
           <div className="max-w-sm mx-auto">
             <div className="mb-6 text-center">
-              <a className="inline-block mb-6" href="#">
-                <img
-                  className="h-16"
-                  src="flex-ui-assets/logos/flex-circle-green.svg"
-                  alt=""
-                />
-              </a>
               <h3 className="mb-4 text-2xl md:text-3xl font-bold">
                 Join our community
               </h3>
@@ -72,6 +65,19 @@ const Register = () => {
                   htmlFor=""
                 >
                   Password*
+                </label>
+                <input
+                  className="appearance-none block w-full p-3 leading-5 text-coolGray-900 border border-coolGray-200 rounded-lg shadow-md placeholder-coolGray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+                  type="password"
+                  placeholder="************"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block mb-2 text-coolGray-800 font-medium"
+                  htmlFor=""
+                >
+                  Confirm Password
                 </label>
                 <input
                   className="appearance-none block w-full p-3 leading-5 text-coolGray-900 border border-coolGray-200 rounded-lg shadow-md placeholder-coolGray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
